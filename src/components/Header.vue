f52a19fa7d318721f37191427f2072a230581ca2
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-06-08 16:04:10 +0200

    Json

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,61 +22,8 @@
 <script>
 export default {
     name: 'Header',
-    data: function() {
-        return {
-            links: [
-                {
-                    text: 'Characters',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Comics',
-                    url: '#',
-                    current: true
-                },
-                {
-                    text: 'Movies',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'TV',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Games',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Collectibles',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Videos',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Fans',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'News',
-                    url: '#',
-                    current: false
-                },
-                {
-                    text: 'Shop',
-                    url: '#',
-                    current: false
-                }
-            ]
-        }
+    props: {
+        links: Array
     }
 }
 </script>
